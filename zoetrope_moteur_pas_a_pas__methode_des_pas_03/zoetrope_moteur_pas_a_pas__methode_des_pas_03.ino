219d09d2670b8e0cf946d2828b3b0ba9535a8083
Philippe Jadin <philippe.jadin@gmail.com>
2021-10-22 14:24:13 +0200

    allow set frames per disk using third pot

---
+++ zoetrope_moteur_pas_a_pas__methode_des_pas_03/zoetrope_moteur_pas_a_pas__methode_des_pas_03.ino
@@ -0,0 +1,184 @@
+/*
+  Version du 15 septembre 2021 (on est toujours là)
+
+
+  Cette méthode consiste simplement à ne pas utiliser le temps comme base de calcul, mais les pas qui font tourner le moteur, y compris pour définir la durée du flash
+  (comptée en nombre de pas et pas en microsecondes)
+
+  Le code est bcp plus simple à comprendre comme cela !
+*/
+
+
+/* HARDWARE pins */
+
+int motor_clock_pin = 3;      // stepper motor clock
+int motor_direction_pin = 2;      // stepper motor direction
+int motor_disable_pin = 4;      // pin to disable the motor data
+int flash_pin = 5; // pin pour le flash
+int step_duration_pin = A0; // potentiometer pin to set motor interval
+int flash_duration_pin = A1; // potentiometer pin to set flash duration
+int frames_pin = A2; // potentiometer pin to set number of frames per disc
+
+
+
+/* Réglages du zoetrope en lui meme */
+
+
+int inactive_area = 100; // neutral zone of the potentiometer (the "off" position of the motor)
+
+int step_duration = 400; // en microsecondes
+int step_duration_min = 0;
+int step_duration_max = 2000;
+
+
+int steps = 400; // Nombre de pas du stepper pour un tour
+int frames = 16; // nombre d'images dans l'animation
+
+
+int frames_per_disc[] = {8, 10, 16, 20, 24, 32, 64}; // frames per disc available to choose from third potentiometer
+
+
+int flash_duration = 1; // en nombre de pas
+int flash_duration_min = 100;
+int flash_duration_max = steps / frames / 2;
+
+
+
+
+/* Variable à usage interne */
+int val = 0;
+int motor_on = false;
+int compteur = 0;
+int flash = 0;
+
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(motor_clock_pin, OUTPUT);
+  pinMode(motor_direction_pin, OUTPUT);
+  pinMode(flash_pin, OUTPUT);
+  pinMode(motor_disable_pin, OUTPUT);
+
+  digitalWrite(motor_disable_pin, LOW);
+
+  Serial.begin(9600);
+
+  digitalWrite(motor_direction_pin, HIGH);
+
+}
+
+void loop()
+{
+  // read user input
+  // set motor speed from potentiometer
+  val = analogRead(step_duration_pin);
+
+
+  if (val < 512 - inactive_area)
+  {
+    step_duration = map(val, 512 - inactive_area, 0, step_duration_max, step_duration_min);
+    digitalWrite(motor_direction_pin, HIGH);
+    digitalWrite(motor_disable_pin, LOW);
+    motor_on = true;
+  }
+  else if (val > 512 + inactive_area)
+  {
+    step_duration = map(val, 512 + inactive_area, 1000, step_duration_max, step_duration_min);
+    digitalWrite(motor_direction_pin, LOW);
+    digitalWrite(motor_disable_pin, LOW);
+    motor_on = true;
+  }
+  else
+  {
+    digitalWrite(motor_disable_pin, HIGH);
+    motor_on = false;
+  }
+
+
+  // set frames from frames potentiometer
+
+  val = analogRead(frames_pin);
+  int index = map(val, 1023, 0, 0, 6);
+
+  frames =  frames_per_disc[index];
+
+
+  //Serial.print("frames : ");
+  //Serial.println(frames);
+
+  flash_duration_max = steps / frames / 2;
+
+  //Serial.print("flash_duration_max : ");
+  //Serial.println(flash_duration_max);
+
+
+  // set flash speed from potentiometer
+
+  val = analogRead(flash_duration_pin);
+
+
+
+  flash_duration = map(val, 1023, 0, 0, flash_duration_max);
+
+  //Serial.print("flash_duration : ");
+  //Serial.println(flash_duration);
+
+  if (motor_on)
+  {
+    /*
+        Serial.print("calc : ");
+        Serial.println(steps / frames);
+    */
+    // handle flash on
+    if (compteur == steps / frames)
+    {
+      flash = 0;
+      compteur = 0;
+      digitalWrite(flash_pin, HIGH);
+      //Serial.println("-------------flash on!-----------------");
+    }
+
+
+    // handle flash off
+    if (flash >= flash_duration)
+    {
+      digitalWrite(flash_pin, LOW);
+      flash = 0;
+      //Serial.println("flash off!");
+
+    }
+
+
+    compteur ++;
+    flash ++;
+
+    digitalWrite(motor_clock_pin, HIGH);
+    delayMicroseconds(step_duration);
+    digitalWrite(motor_clock_pin, LOW);
+
+    /*
+        Serial.print("compteur : ");
+        Serial.println(compteur);
+
+        Serial.print("flash : ");
+        Serial.println(flash);
+
+        Serial.print("flash duration: ");
+        Serial.println(flash_duration);
+    */
+
+    //delay(300);
+
+
+  }
+  else // mode standby
+  {
+    flash = 0;
+    compteur = 0;
+
+    digitalWrite(flash_pin, HIGH);
+    delayMicroseconds(flash_duration * 20);
+    digitalWrite(flash_pin, LOW);
+    delayMicroseconds(1000);
+  }
+}
